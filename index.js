b36215a2a2b03f004d1eac10f4a6cc79c7717d01
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-06-17 16:42:38 +0530

    chore: Update json-server library to latest version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router('db.json'); // Assuming you have a db.json file in your project
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000; // you can use any port number here; i chose to use 3000
 
 server.use(middlewares);
 server.use(router);
-
-server.listen(port);
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
